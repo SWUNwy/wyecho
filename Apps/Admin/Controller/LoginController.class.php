698d68acdd97c6d5cd1b4a675624d1b315b673bb
wangyu <swunwy@163.com>
2017-12-04 23:04:43 +0800

    update

--- Apps/Admin/Controller/LoginController.class.php
+++ Apps/Admin/Controller/LoginController.class.php
@@ -22,7 +22,27 @@ class LoginController extends Controller {
     }
     
     public function login() {
-        $this->redirect('Index/index');
+
+        $verify = new \Think\Verify();
+        if(!$verify->check(I('verify'))){
+            $this->error("验证码不正确!");
+        }        
+        $user = M('user')->where(array('uname' => I('uname','','htmlspecialchars')))->find();
+        if (!$user || $user['pwd'] != I('pwd', '', 'MD5')) {
+            $this->error('用户名或密码错误');
+        }
+
+        $data = array(
+            'last_time' => date('Y-m-d H:i:s'),
+            'last_ip' => get_client_ip()
+        );
+        $db = M('user')->where('id='.$user['id'])->save($data);
+
+        //写入session
+        session('admin_id',$user['id']);
+        session('admin_username',$res['username']);
+        $this->success("登录成功!",U('Index/index'));
+
     }
     
     public function logout() {
