3b6d86c475454065c524aba5bf57d69987280fdc
wangyu <swunwy@163.com>
2017-12-10 15:34:17 +0800

    update

--- Apps/Admin/Controller/UserController.class.php
+++ Apps/Admin/Controller/UserController.class.php
@@ -12,8 +12,8 @@ class UserController extends Controller {
 		import('Org.Util.Page');
 
 		$count = $user->count();
-        //实例化分页类 传入总记录数和每页显示的记录数
-        $Page = new \Think\Page($count,10);
+		        //实例化分页类 传入总记录数和每页显示的记录数
+		$Page = new \Think\Page($count,10);
 
         //分页显示输出
         $show = $Page->show();
