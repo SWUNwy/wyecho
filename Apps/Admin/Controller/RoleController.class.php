d37150e697fd460f8a4af7b2368c04125b09db09
wangyu <swunwy@163.com>
2017-12-12 23:27:57 +0800

    update

--- Apps/Admin/Controller/RoleController.class.php
+++ Apps/Admin/Controller/RoleController.class.php
@@ -11,6 +11,18 @@ use Think\Controller;
 class RoleController extends Controller {
     //put your code here
     public function index() {
+        $role = M('role');
+        import('Org.Util.Page');
+
+        $count = $role->count();
+                //实例化分页类 传入总记录数和每页显示的记录数
+        $Page = new \Think\Page($count,10);
+
+        //分页显示输出
+        $show = $Page->show();
+        $list = $role->order('last_time desc')->limit($Page->firstRow.','.$Page->listRows)->select();
+        $this->assign('list',$list);
+        $this->assign('page',$show);                
         $this->display();
     }
     
