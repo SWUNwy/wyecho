b0a79011fedef9b7c5aaddebb6986f3ff071ff00
wangyu <swunwy@163.com>
2018-01-26 23:58:36 +0800

    update

---
+++ Apps/Home/Model/CommonModel.class.php
@@ -0,0 +1,22 @@
+<?php
+// +----------------------------------------------------------------------
+// | wyecho
+// +----------------------------------------------------------------------
+// | Copyright (c) 2018~2022  All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: wyecho <@sina.com>
+// +----------------------------------------------------------------------
+namespace Home\Model;
+
+use Think\Model;
+
+/**
+ * Description of CommonModel
+ *
+ * @author Administrator
+ */
+class CommonModel extends Model {
+    //put your code here
+}
