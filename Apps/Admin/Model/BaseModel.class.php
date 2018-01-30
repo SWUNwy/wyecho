8ca3507e5f719de9f5833b6738256f647be62408
wangyu <swunwy@163.com>
2018-01-30 22:48:34 +0800

    update

---
+++ Apps/Admin/Model/BaseModel.class.php
@@ -0,0 +1,21 @@
+<?php
+// +----------------------------------------------------------------------
+// | wyecho
+// +----------------------------------------------------------------------
+// | Copyright (c) 2018~2022  All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: wyecho <@sina.com>
+// +----------------------------------------------------------------------
+namespace Admin\Model;
+
+use Think\Model;
+/**
+ * Description of BaseModel
+ *
+ * @author Administrator
+ */
+class BaseModel extends Model {
+    //put your code here
+}
---
+++ Apps/Admin/Model/BlogModel.class.php
@@ -0,0 +1,21 @@
+<?php
+// +----------------------------------------------------------------------
+// | wyecho
+// +----------------------------------------------------------------------
+// | Copyright (c) 2018~2022  All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: wyecho <@sina.com>
+// +----------------------------------------------------------------------
+namespace Admin\Model;
+
+use Think\Model;
+/**
+ * Description of BlogModel
+ * 博客模型
+ * @author Administrator
+ */
+class BlogModel extends Model {
+    //put your code here
+}
