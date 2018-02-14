9ee8c04b56b3ab5917b3aa048f9b118fc2936887
wangyu <swunwy@163.com>
2018-02-14 20:44:20 +0800

    update

--- Runtime/Cache/Home/cdb9178de0ed10054c9a15bc5887b829.php
+++ Runtime/Cache/Home/cdb9178de0ed10054c9a15bc5887b829.php
@@ -49,25 +49,19 @@
         <div class="layui-container fly-marginTop fly-user-main">
             <ul class="layui-nav layui-nav-tree layui-inline" lay-filter="user">
                 <li class="layui-nav-item">
-                    <a href="home.html">
-                        <i class="layui-icon">&#xe609;</i>
-                        我的主页
-                    </a>
-                </li>
-                <li class="layui-nav-item">
-                    <a href="index.html">
+                    <a href="<?php echo U('user/index');?>">
                         <i class="layui-icon">&#xe612;</i>
                         用户中心
                     </a>
                 </li>
                 <li class="layui-nav-item">
-                    <a href="set.html">
+                    <a href="<?php echo U('User/set');?>">
                         <i class="layui-icon">&#xe620;</i>
                         基本设置
                     </a>
                 </li>
                 <li class="layui-nav-item layui-this">
-                    <a href="message.html">
+                    <a href="<?php echo U('User/message');?>">
                         <i class="layui-icon">&#xe611;</i>
                         我的消息
                     </a>
