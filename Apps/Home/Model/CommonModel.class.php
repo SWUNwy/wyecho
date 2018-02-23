c2a44b261c6c5cb063383038c14d6849b2d5ad1c
wangyu <swunwy@163.com>
2018-02-23 23:50:08 +0800

    update

--- Apps/Home/Model/CommonModel.class.php
+++ Apps/Home/Model/CommonModel.class.php
@@ -19,11 +19,21 @@ use Think\Model;
  */
 class CommonModel extends Model {
     //put your code here
+    /**
+     * [add description]
+     * @param char  $table 需要增加数据的表
+     * @param array $data  需要增加的数据
+     * @return boolean 操作结果
+     */
+    public function add() {
+    	return true;
+    }
+
     public function save() {
-    	return ture;
+    	return true;
     }
 
     public function delete() {
-    	return ture;
+    	return true;
     }
 }
