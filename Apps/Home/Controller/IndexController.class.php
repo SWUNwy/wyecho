390630416a369c1d629ff0aa714d75c1ed0dcb5d
wangyu <swunwy@163.com>
2018-04-01 23:31:02 +0800

    update IndexController

--- Apps/Home/Controller/IndexController.class.php
+++ Apps/Home/Controller/IndexController.class.php
@@ -20,10 +20,24 @@ use Home\Model\BlogModel;
 class IndexController extends Controller {
 
     public function index() {
+
+    	$db = M('blog');
+    	// 导入分页类
+        import('Org.Util.Page');
     	
-		$data = new BlogModel();
-		$data = $data->blogList();
+    	$count = $db->count();
+        //实例化分页类 传入总记录数和每页显示的记录数
+        $Page = new \Think\Page($count,10);
+
+        //分页显示输出
+        $show = $Page->show();
+        $data = $db->order('last_time desc')->limit($Page->firstRow.','.$Page->listRows)->select();
+		// $data = new BlogModel();
+		// $data = $data->blogList();
+		var_dump($show);
+		die();
 		$this->assign('data',$data);
+		$this->assign('page',$page);
 		$this->display();
 
     }
--- Runtime/Cache/Home/400c995c330f1f6ecac6b9102849eb7e.php
+++ Runtime/Cache/Home/400c995c330f1f6ecac6b9102849eb7e.php
@@ -268,7 +268,7 @@
                         </ul>
                         
                         <div style="text-align: center">
-                            <div class="laypage-main"><span class="laypage-curr">1</span><a href="/jie/page/2/">2</a><a href="/jie/page/3/">3</a><a href="/jie/page/4/">4</a><a href="/jie/page/5/">5</a><span>…</span><a href="/jie/page/2/" class="laypage-next">下一页</a><a href="/jie/page/148/" class="laypage-last" title="尾页">尾页</a></div>
+                            <div class="laypage-main"><?php echo ($page); ?></div>
                         </div>
 
                     </div>
